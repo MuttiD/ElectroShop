05db5119e8bd7f44bdc1209d65575ee26441e7bb
MuttiD <dgmutti@hotmail.com>
2023-03-31 12:45:43 +0000

    Add shopping bag app, urls and basic html

---
+++ bag/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def view_bag(request):
+    """ A view the renders the bag content """
+
+    return render(request, 'bag/bag.html')
