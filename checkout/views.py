36757e6279b020e3cfb7a2da160d8c7e6a3fc22a
MuttiD <dgmutti@hotmail.com>
2023-04-20 13:29:52 +0000

    Complete webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,7 @@ import json
 @require_POST
 def cache_checkout_data(request):
     try:
+        # Payment id = pid
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
@@ -49,7 +50,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -118,7 +123,7 @@ def checkout(request):
 
 
 def checkout_success(request, order_number):
-    """ 
+    """
     Handle successful checkout
     """
     save_info = request.session.get('save_info')
@@ -126,10 +131,10 @@ def checkout_success(request, order_number):
     messages.success(request, f'Order has been processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order_email}')
-    
+
     if 'bag' in request.session:
         del request.session['bag']
-    
+
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
