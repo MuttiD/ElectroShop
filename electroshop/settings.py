1fbb9d6075482ba0cb6d5e4173bddcb9295e896f
MuttiD <dgmutti@hotmail.com>
2023-04-23 00:44:21 +0000

    Deployment Heroku App

--- electroshop/settings.py
+++ electroshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 if os.path.exists("env.py"):
     import env
 
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-f^fll@7b3ahi0xt(-&+rjk7mxkoy7-0aibf0(gsiw95m%9*6!%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://app-electro.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,13 +127,21 @@ WSGI_APPLICATION = 'electroshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
+#DATABASES = {
+#     'default': dj_database_url.parse('postgres://ylbxnvzp:silZdU2VSIuZcY52oAjf6UlGsweHHwBM@kandula.db.elephantsql.com/ylbxnvzp')
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
